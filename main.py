fcc78bb24eb2eebe4d4ff4407148eaca225856c9
Rafael Cenzano <32753063+RafaelCenzano@users.noreply.github.com>
2019-09-16 08:33:30 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from workshop import app
+
+# Run Flask app
+app.run(
+    host='0.0.0.0', # host to view from outside the network
+    port=5000, # assign to port 5000
+    debug=True # Have debug pages show when there is an error
+)
