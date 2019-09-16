c6e33d3debb9e1c3e08eea11f1e55f603ad5b9d6
Savagecoder77 <savagecoder77@gmail.com>
2019-09-15 23:09:55 -0700

    add advanced route

--- workshop/views.py
+++ workshop/views.py
@@ -7,6 +7,10 @@ from flask import render_template, redirect, url_for
 def index():
     return render_template('index.html')
 
+@app.route("advanced", methods=['GET'])
+def advanced():
+    return render_template('advanced.html')
+
 # Error Handelers
 @app.errorhandler(404)
 def page_not_found(e):
